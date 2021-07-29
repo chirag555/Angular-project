4811b014c3b7e974673dbffcd71a46b41a0dc965
nisarg_parekh <nisyparekh@gmail.com>
2021-07-29 11:52:49 +0530

    delete file

--- Typescript/Interfaces/CricketCoach.ts
+++
@@ -1,6 +0,0 @@
-import{Coach} from './Coach';
-export class CricketCoach implements Coach{
-    getDailyWorkout(): string {
-        return " Practice Spin Bowling";
-    }
-}
--- Typescript/Interfaces/HockeyCoach.ts
+++
@@ -1,7 +0,0 @@
-import{Coach} from './Coach';
-export class HockeyCoach implements Coach{
-    getDailyWorkout(): string {
-        return " Run 10 KM ";
-    }
-   
-}
